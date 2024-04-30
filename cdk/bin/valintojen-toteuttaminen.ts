ea02563d2869efc7e37ec850d437f8d959ad4a73
Risto Salama <risto.salama@gofore.com>
2024-04-30 12:23:19 +0300

    Initial version of cdk deploy

---
+++ cdk/bin/valintojen-toteuttaminen.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { SovellusStack } from '../lib/sovellus-stack';
+
+const app = new cdk.App();
+const environmentName = app.node.tryGetContext('environment');
+
+new SovellusStack(app, 'SovellusStack', {
+  stackName: `${environmentName}-viestinvalitys-sovellus`,
+  environmentName: environmentName,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
