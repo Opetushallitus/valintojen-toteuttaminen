c2fc8356830da3e103908db30f2bb5fa04a548d0
Risto Salama <risto.salama@gofore.com>
2024-05-06 16:20:14 +0300

    Use layout for navigation hakukohde

--- src/app/haku/[oid]/layout.tsx
+++ src/app/haku/[oid]/layout.tsx
@@ -1,7 +1,19 @@
+import HakukohdeList from './hakukohde-list';
+
 export default function HakuLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { oid: string };
 }) {
-  return <section>{children}</section>;
+  return (
+    <main
+      className="mainContainer"
+      style={{ display: 'flex', flexDirection: 'row' }}
+    >
+      <HakukohdeList oid={params.oid} />
+      {children}
+    </main>
+  );
 }
--- src/app/haku/[oid]/page.tsx
+++ src/app/haku/[oid]/page.tsx
@@ -1,32 +1,9 @@
 'use client';
 
-import { useState } from 'react';
-import HakukohdeList from './hakukohde-list';
-import HakukohdeTabs from './hakukohde/[hakukohde]/hakukohde-tabs';
-import { Hakukohde } from '@/app/lib/kouta-types';
-
-export default function HakuPage({ params }: { params: { oid: string } }) {
-  const [selectedHakukohde, setSelectedHakukohde] = useState<Hakukohde | null>(
-    null,
-  );
-
-  const selectHakukohde = (hakukohde: Hakukohde) => {
-    setSelectedHakukohde(hakukohde);
-    history.pushState(null, '', `${params.oid}/hakukohde/${hakukohde.oid}`);
-  };
-
+export default function HakuPage() {
   return (
-    <main
-      className="mainContainer"
-      style={{ display: 'flex', flexDirection: 'row' }}
-    >
-      <HakukohdeList oid={params.oid} selectFn={selectHakukohde} />
-      {selectedHakukohde == null && (
-        <div style={{ alignSelf: 'center', width: '70%' }}>
-          <h2>Valitse hakukohde</h2>
-        </div>
-      )}
-      {selectedHakukohde && <HakukohdeTabs hakukohde={selectedHakukohde} />}
-    </main>
+    <div style={{ alignSelf: 'center', width: '70%' }}>
+      <h2>Valitse hakukohde</h2>
+    </div>
   );
 }
