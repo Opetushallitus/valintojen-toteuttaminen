6dac0fc947675b9730e9ed69693891f06c9e1cf9
Risto Salama <risto.salama@gofore.com>
2024-05-08 10:47:08 +0300

    Add pull request template

---
+++ pull_request_template.md
@@ -0,0 +1,3 @@
+Oletko lisännyt tarvittavat yksikkö- tai ui-testit toiminnallisuudelle? Kyllä/En
+Oletko tarkistanut ja päivittänyt riippuvuudet? Kyllä/En
+Oletko kokeillut toimiiko käyttöliittymä mobiilissa landscape moodissa? Kyllä/En/Ei koske
--- src/app/haku/[oid]/page.tsx
+++ src/app/haku/[oid]/page.tsx
@@ -2,7 +2,7 @@
 
 export default function HakuPage() {
   return (
-    <div style={{ alignSelf: 'center', width: '70%' }}>
+    <div style={{ alignSelf: 'center', width: '70%', padding: '1rem 2rem' }}>
       <h2>Valitse hakukohde</h2>
     </div>
   );
