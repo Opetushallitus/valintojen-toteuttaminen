cabd7c420e46bd59ea8385252d2425afa37eba8b
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-05-15 09:58:20 +0300

    OK-508: Lisätty yksikkötestit getSortParts-funktiolle

---
+++ src/app/components/table/getSortParts.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from 'vitest';
+import { getSortParts } from './getSortParts';
+
+const EMPTY_RESULT = {
+  orderBy: undefined,
+  direction: undefined,
+};
+
+test('getSortParts', () => {
+  expect(getSortParts('col:asc', 'col')).toEqual({
+    orderBy: 'col',
+    direction: 'asc',
+  });
+  expect(getSortParts('col:asc')).toEqual({
+    orderBy: 'col',
+    direction: 'asc',
+  });
+  expect(getSortParts('col:desc', 'col')).toEqual({
+    orderBy: 'col',
+    direction: 'desc',
+  });
+  expect(getSortParts('', 'col')).toEqual(EMPTY_RESULT);
+  expect(getSortParts(undefined, 'col')).toEqual(EMPTY_RESULT);
+});
