49dcb8deb0bdba72b41f8dddee5b80d4884931f2
Risto Salama <risto.salama@gofore.com>
2024-06-05 10:26:28 +0300

    WIP close hakukohdepanel

--- src/app/haku/[oid]/hakukohde-panel.tsx
+++ src/app/haku/[oid]/hakukohde-panel.tsx
@@ -1,21 +1,37 @@
 'use client';
 
-import { styled } from '@mui/material';
+import { styled, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import HakukohdeList from './hakukohde-list';
 import HakukohdeSearch from './hakukohde-search';
+import { useState } from 'react';
 
 const StyledPanel = styled('div')({
   width: '20vw',
   textAlign: 'left',
   minHeight: '85vh',
+  position: 'relative',
+});
+
+const MinimizedPanel = styled('div')({
+  width: '5vw'
 });
 
 export const HakukohdePanel = ({ oid }: { oid: string }) => {
+
+  const [ minimized, setMinimized ] = useState(false);
+
   return (
-    <StyledPanel>
-      <HakukohdeSearch />
-      <HakukohdeList hakuOid={oid} />
-    </StyledPanel>
+    <>
+      {!minimized &&
+        <StyledPanel>
+          <IconButton sx={{position: 'absolute', top: '5px', right: '5px'}}><CloseIcon /></IconButton>
+          <HakukohdeSearch />
+          <HakukohdeList hakuOid={oid} />
+        </StyledPanel>
+      }
+    </>
+
   );
 };
 
