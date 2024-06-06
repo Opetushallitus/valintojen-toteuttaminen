f223957c730010d35bbe0d141abae15d2e6b1444
Risto Salama <risto.salama@gofore.com>
2024-06-06 10:41:42 +0300

    Add some styles to empty haku page

--- src/app/haku/[oid]/page.tsx
+++ src/app/haku/[oid]/page.tsx
@@ -1,13 +1,29 @@
 'use client';
 
 import { useTranslations } from '@/app/hooks/useTranslations';
+import { colors } from '@/app/theme';
+import { ListAlt } from '@mui/icons-material';
+import { styled } from '@mui/material';
 
 export default function HakuPage() {
   const { t } = useTranslations();
 
+  const Container = styled('div')({
+    textAlign: 'center',
+    padding: '5rem 2rem 1rem',
+    width: '70%',
+  });
+
   return (
-    <div style={{ alignSelf: 'center', width: '70%', padding: '1rem 2rem' }}>
+    <Container>
+      <ListAlt
+        sx={{
+          borderRadius: '45px',
+          backgroundColor: colors.grey50,
+          padding: '15px',
+        }}
+      />
       <h2>{t('hakukohde.valitse')}</h2>
-    </div>
+    </Container>
   );
 }
