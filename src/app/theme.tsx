8a5a7077fe9c5e1683ee524633645222914b06ec
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-06-11 13:44:36 +0300

    OK-496: Käytetään oph-design-system teemaa

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,205 +1,33 @@
 'use client';
-import { ButtonOwnProps } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import NextLink, { LinkProps } from 'next/link';
-import { Open_Sans } from 'next/font/google';
 import * as React from 'react';
+import { MUI_NEXTJS_OVERRIDES } from '@opetushallitus/oph-design-system/next/theme';
 
-const LinkBehaviour = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  function LinkBehaviour(props, ref) {
-    return <NextLink ref={ref} {...props} />;
-  },
-);
+import { createODSTheme } from '@opetushallitus/oph-design-system/theme';
+import { deepmerge } from '@mui/utils';
 
-export const colors = {
-  grey900: '#1D1D1D',
-  grey800: '#454545',
-  grey700: '#4C4C4C',
-  grey600: '#5D5D5D',
-  grey500: '#6D6D6D',
-  grey400: '#B2B2B2',
-  grey300: '#C8C8C8',
-  grey200: '#DFDFDF',
-  grey100: '#EDEDED',
-  grey50: '#F6F6F6',
+import { colors } from '@opetushallitus/oph-design-system';
 
-  white: '#FFFFFF',
-  black: '#000000',
+export { colors };
 
-  blue1: '#000066',
-  blue2: '#0033CC',
-  blue3: '#0041DC',
-  cyan1: '#006699',
-  cyan2: '#66CCCC',
-  cyan3: '#99FFFF',
-  lightBlue1: '#82D4FF',
-  lightBlue2: '#C1EAFF',
-};
-
-const openSans = Open_Sans({
-  weight: ['400', '600', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+const theme = createODSTheme({
+  variant: 'oph',
+  overrides: deepmerge(MUI_NEXTJS_OVERRIDES, {}),
 });
 
-const theme = createTheme({
-  typography: {
-    fontFamily: openSans.style.fontFamily,
-  },
-  palette: {
-    primary: {
-      main: colors.blue2,
-      light: colors.blue3,
-      dark: colors.blue1,
-      contrastText: colors.white,
-    },
-  },
-  components: {
-    MuiLink: {
-      defaultProps: {
-        component: LinkBehaviour,
-      },
-    },
-    MuiButton: {
-      defaultProps: {
-        disableRipple: true,
-      },
-      styleOverrides: {
-        root: {
-          boxShadow: 'none',
-          textTransform: 'none',
-          fontWeight: '600',
-        },
-        contained: ({ ownerState, theme }) => {
-          return {
-            borderRadius: '2px',
-            '&:hover': {
-              backgroundColor: getColorByName(ownerState.color, theme, 'light'),
-            },
-            '&:active': {
-              backgroundColor: getColorByName(ownerState.color, theme, 'dark'),
-            },
-          };
-        },
-        outlined: ({ ownerState, theme }) => {
-          return {
-            color: getColorByName(ownerState.color, theme, 'main'),
-            borderRadius: '2px',
-            border: `3px solid ${getColorByName(ownerState.color, theme, 'main')}`,
-            '&:hover': {
-              borderWidth: '3px',
-              backgroundColor: colors.white,
-              color: getColorByName(ownerState.color, theme, 'light'),
-              borderColor: getColorByName(ownerState.color, theme, 'light'),
-            },
-            '&:active': {
-              borderWidth: '3px',
-              backgroundColor: colors.white,
-              color: getColorByName(ownerState.color, theme, 'dark'),
-              borderColor: getColorByName(ownerState.color, theme, 'dark'),
-            },
-          };
-        },
-        text: ({ ownerState, theme }) => {
-          return {
-            color: getColorByName(ownerState.color, theme, 'main'),
-            background: 'none',
-            '&:hover': {
-              color: getColorByName(ownerState.color, theme, 'light'),
-              background: 'none',
-            },
-            '&:active': {
-              color: getColorByName(ownerState.color, theme, 'dark'),
-              background: 'none',
-            },
-          };
-        },
-      },
-    },
-    MuiFormLabel: {
-      styleOverrides: {
-        root: {
-          color: colors.black,
-          '&.Mui-focused': {
-            color: colors.black,
-          },
-        },
-      },
-    },
-    MuiInputLabel: {
-      styleOverrides: {
-        root: {
-          color: colors.black,
-          '&.Mui-focused': {
-            color: colors.black,
-          },
-        },
-      },
-    },
-    MuiPagination: {
-      defaultProps: {
-        variant: 'text',
-        shape: 'rounded',
-      },
-    },
-    MuiPaginationItem: {
-      styleOverrides: {
-        root: {
-          borderRadius: '2px',
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: ({ theme }) => {
-          return {
-            '.MuiOutlinedInput-notchedOutline': {
-              borderRadius: '2px',
-              borderWidth: '1px',
-            },
-            '&:hover .MuiOutlinedInput-notchedOutline': {
-              borderWidth: '2px',
-              borderColor: theme.palette.primary.main,
-            },
-          };
-        },
-      },
-    },
-    MuiFormControl: {
-      defaultProps: {
-        size: 'small',
-      },
-    },
-    MuiMenuItem: {
-      styleOverrides: {
-        root: ({ theme }) => {
-          return {
-            '&.Mui-selected': {
-              backgroundColor: colors.grey50,
-              color: theme.palette.primary.main,
-              '&:hover': {
-                backgroundColor: colors.grey50,
-              },
-            },
-            '&.Mui-selected.Mui-focusVisible': {
-              backgroundColor: colors.grey50,
-              color: theme.palette.primary.main,
-            },
-          };
-        },
-      },
-    },
-  },
-});
-
-function getColorByName(
-  colorName: ButtonOwnProps['color'],
-  customTheme: typeof theme,
-  mode: 'main' | 'light' | 'dark',
+// MUI:sta (Emotionista) puuttuu styled-componentsin .attrs
+// Tällä voi asettaa oletus-propsit ilman, että tarvii luoda välikomponenttia
+export function withDefaultProps<P>(
+  Component: React.ComponentType<P>,
+  defaultProps: Partial<P>,
+  displayName = 'ComponentWithDefaultProps',
 ) {
-  return colorName === 'inherit'
-    ? 'inherit'
-    : customTheme.palette[colorName ?? 'primary'][mode];
+  const ComponentWithDefaultProps = React.forwardRef<
+    React.ComponentRef<React.ComponentType<P>>,
+    P
+  >((props, ref) => <Component {...defaultProps} {...props} ref={ref} />);
+
+  ComponentWithDefaultProps.displayName = displayName;
+  return ComponentWithDefaultProps;
 }
 
 export default theme;
