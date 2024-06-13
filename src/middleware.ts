ff098c72032b07a61d51279449bb8a5d51dcadc5
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-06-13 13:44:51 +0300

    OK-538: Käytetään "cdk-nextjs-standalone"-konstruktia itse tehdyn Next.js infran sijaan
    - Konstruktin käyttäminen korjaa sivun latautumisen uudelleen kun pitäisi tehdä osittainen renderöinti. Jollain tavalla itse toteutettu infra on puutteellinen.
    - Ei enää tarvetta lisätä puuttuvaa basePathia middlewaressa, koska kaikki lölytyy /valintojen-toteuttaminen-polusta

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { isLocalhost } from './app/lib/configuration';
 
 export async function middleware(req: NextRequest) {
   const requestURL = new URL(req.url);
@@ -9,26 +8,6 @@ export async function middleware(req: NextRequest) {
   if (ticket !== '') {
     requestURL.searchParams.delete('ticket');
     return NextResponse.redirect(requestURL);
-  }
-
-  if (!isLocalhost) {
-    // If X-Forwarded-Host is not set, then don't bother with middleware.
-    const forwardedHost = req.headers.get('X-Forwarded-Host');
-    if (forwardedHost === null) {
-      return NextResponse.next();
-    }
-
-    // Middleware starts here
-    const requestedUrl = req.nextUrl.clone();
-    const basePath = process.env.basePath ?? '';
-
-    if (!requestedUrl.pathname.includes(basePath)) {
-      requestedUrl.pathname = basePath + requestedUrl.pathname;
-      console.log(
-        `basePath not detected in request URL. Forwarding to ${requestedUrl.toString()}`,
-      );
-      return NextResponse.rewrite(requestedUrl);
-    }
   }
 
   return NextResponse.next();
