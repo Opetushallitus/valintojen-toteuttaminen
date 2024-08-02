2c855fe31e01130cd7b0dd5ef3dc04ab30f8a23a
Risto Salama <risto.salama@gofore.com>
2024-08-02 11:21:58 +0300

    Initial pistesyotto table and search

---
+++ src/app/haku/[oid]/hakukohde/[hakukohde]/pistesyotto/pistesyotto-search.tsx
@@ -0,0 +1,47 @@
+import { usePisteSyottoSearchParams } from '@/app/hooks/usePisteSyottoSearch';
+import { useTranslations } from '@/app/hooks/useTranslations';
+import { Search } from '@mui/icons-material';
+import {
+  FormControl,
+  FormLabel,
+  InputAdornment,
+  OutlinedInput,
+} from '@mui/material';
+import { ChangeEvent } from 'react';
+
+export const PisteSyottoSearch = () => {
+  const { searchPhrase, setSearchPhrase } = usePisteSyottoSearchParams();
+  const { t } = useTranslations();
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchPhrase(e.target.value);
+  };
+
+  return (
+    <FormControl
+      sx={{
+        flexGrow: 4,
+        minWidth: '380px',
+        textAlign: 'left',
+      }}
+    >
+      <FormLabel htmlFor="hakeneet-search" sx={{ fontWeight: 600 }}>
+        {t('hakeneet.hae')}
+      </FormLabel>
+      <OutlinedInput
+        id="hakeneet-search"
+        name="hakeneet-search"
+        defaultValue={searchPhrase}
+        onChange={handleSearchChange}
+        autoFocus={true}
+        type="text"
+        endAdornment={
+          <InputAdornment position="end">
+            <Search />
+          </InputAdornment>
+        }
+      />
+    </FormControl>
+  );
+};
+
+export default PisteSyottoSearch;
