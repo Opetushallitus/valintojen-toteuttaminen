8c5bd9e9dd82c3f187b513cf5c9ac224879b866f
Risto Salama <risto.salama@gofore.com>
2024-09-16 13:03:25 +0300

    Use provide instead of setup on pistesyotto machine

--- src/app/haku/[oid]/hakukohde/[hakukohde]/pistesyotto/pistesyotto-state.ts
+++ src/app/haku/[oid]/hakukohde/[hakukohde]/pistesyotto/pistesyotto-state.ts
@@ -4,11 +4,12 @@ import {
   ValintakoeOsallistuminen,
 } from '@/app/lib/types/laskenta-types';
 import { updateScoresForHakukohde } from '@/app/lib/valintalaskentakoostepalvelu';
-import { assign, fromPromise, setup } from 'xstate';
+import { assign, createMachine, fromPromise } from 'xstate';
 
 export type PisteSyottoContext = {
   pistetiedot: HakemuksenPistetiedot[];
   changedPistetiedot: HakemuksenPistetiedot[];
+  toastMessage?: string;
 };
 
 export enum PisteSyottoStates {
@@ -29,41 +30,13 @@ export const createPisteSyottoMachine = (
   pistetiedot: HakemuksenPistetiedot[],
   addToast: (toast: Toast) => void,
 ) => {
-  return setup({
-    types: {
-      context: {
-        pistetiedot: [],
-        changedPistetiedot: [],
-      } as PisteSyottoContext,
-    },
-    guards: {
-      hasChangedPistetiedot: ({ context }) =>
-        context.changedPistetiedot.length > 0,
-    },
-    actions: {
-      //eslint-disable-next-line no-empty-pattern
-      alert: ({}, params: { message: string }) => {
-        addToast({
-          key: `pistetiedot-update-failed-for-${hakukohdeOid}`,
-          message: params.message,
-          type: 'error',
-        });
-      },
-    },
-    actors: {
-      updatePistetiedot: fromPromise(
-        ({ input }: { input: HakemuksenPistetiedot[] }) => {
-          return updateScoresForHakukohde(hakuOid, hakukohdeOid, input);
-        },
-      ),
-    },
-  }).createMachine({
+  const pisteMachine = createMachine({
     id: `PistesyottoMachine-${hakukohdeOid}`,
     initial: PisteSyottoStates.IDLE,
     context: {
       pistetiedot: pistetiedot,
       changedPistetiedot: [],
-    },
+    } as PisteSyottoContext,
     states: {
       [PisteSyottoStates.IDLE]: {
         on: {
@@ -161,6 +134,27 @@ export const createPisteSyottoMachine = (
           },
         ],
       },
+    },
+  });
+  return pisteMachine.provide({
+    guards: {
+      hasChangedPistetiedot: ({ context }) =>
+        context.changedPistetiedot.length > 0,
+    },
+    actions: {
+      alert: (_, params) =>
+        addToast({
+          key: `pistetiedot-update-failed-for-${hakukohdeOid}`,
+          message: (params as { message: string }).message,
+          type: 'error',
+        }),
+    },
+    actors: {
+      updatePistetiedot: fromPromise(
+        ({ input }: { input: HakemuksenPistetiedot[] }) => {
+          return updateScoresForHakukohde(hakuOid, hakukohdeOid, input);
+        },
+      ),
     },
   });
 };
