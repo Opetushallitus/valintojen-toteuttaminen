4b38c90c2a44a2ff9fe62c998e05c781bc161fa1
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-09-17 11:01:22 +0300

    OK-509: Siirretty suurin osa teeman overrideista oph-design-systemiin

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,9 +1,8 @@
 'use client';
 import { ophColors } from '@opetushallitus/oph-design-system';
+import { styled as muiStyled } from '@mui/material/styles';
 
 export { ophColors } from '@opetushallitus/oph-design-system';
-
-import { styled as muiStyled } from '@mui/material/styles';
 
 const withTransientProps = (propName: string) =>
   // Emotion doesn't support transient props by default so add support manually
@@ -21,67 +20,6 @@ export const styled: typeof muiStyled = (
 
 export const THEME_OVERRIDES = {
   components: {
-    MuiFormLabel: {
-      styleOverrides: {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        root: ({ theme }: any) => ({
-          ...theme.typography.label,
-        }),
-      },
-    },
-    MuiToggleButton: {
-      defaultProps: {
-        disableRipple: true,
-      },
-      styleOverrides: {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        root: ({ theme }: any) => ({
-          paddingTop: 0,
-          paddingBottom: 0,
-          color: ophColors.grey900,
-          borderColor: theme.palette.primary.main,
-          borderWidth: '2px',
-          borderRadius: '2px',
-          '&.Mui-selected': {
-            backgroundColor: theme.palette.primary.main,
-            color: ophColors.white,
-            '&:hover': {
-              backgroundColor: theme.palette.primary.light,
-              color: ophColors.white,
-            },
-            '&:active, &.Mui-focusVisible': {
-              backgroundColor: theme.palette.primary.dark,
-            },
-          },
-          '&:hover': {
-            backgroundColor: ophColors.white,
-            borderColor: theme.palette.primary.light,
-            color: theme.palette.primary.main,
-          },
-          '&.Mui-disabled': {},
-        }),
-      },
-    },
-    MuiCheckbox: {
-      defaultProps: {
-        disableRipple: true,
-      },
-    },
-    MuiButtonBase: {
-      defaultProps: {
-        disableRipple: true,
-      },
-    },
-    MuiAccordion: {
-      defaultProps: {
-        disableGutters: true,
-      },
-      styleOverrides: {
-        root: {
-          boxShadow: 'none',
-        },
-      },
-    },
     MuiInputBase: {
       styleOverrides: {
         root: {
@@ -101,5 +39,5 @@ export const THEME_OVERRIDES = {
         },
       },
     },
-  },
+  }
 };
