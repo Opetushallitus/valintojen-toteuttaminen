2610c6eaa759825791849c6c29d466fc4da2effb
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-10-09 18:13:57 +0300

    Parannettu hakukohteet-sivupalkin tyylejä
    - Sivupalkki sticky:ksi
    - Sivupalkin näyttö- ja piilotus-napit käyttämään OphButton-komponenttia
    - Muutettu palkin näyttämis-nappi korkeammaksi ja muokkailtu tyylit paremmin proton mukaiseksi
    - Näytetään hakukohteiden määrä vierityksen ulkopuolella

--- src/app/haku/[oid]/components/hakukohde-list.tsx
+++ src/app/haku/[oid]/components/hakukohde-list.tsx
@@ -15,7 +15,8 @@ const StyledList = styled('nav')(({ theme }) => ({
   width: '100%',
   textAlign: 'left',
   overflowY: 'auto',
-  height: '80vh',
+  height: 'auto',
+  paddingRight: theme.spacing(0.5),
   gap: theme.spacing(0.5),
 }));
 
@@ -44,32 +45,34 @@ export const HakukohdeList = ({ hakuOid }: { hakuOid: string }) => {
   const activeHakukohdeTab = useHakukohdeTab();
   const selectedHakukohdeOid = useSelectedHakukohdeOid();
   return (
-    <StyledList tabIndex={0} aria-label={t('hakukohde.navigaatio')}>
+    <>
       <OphTypography>
         {results.length} {t('haku.hakukohdetta')}
       </OphTypography>
-      {results?.map((hk: Hakukohde) => (
-        <StyledItem
-          key={hk.oid}
-          className={selectedHakukohdeOid === hk.oid ? SELECTED_CLASS : ''}
-          href={`/haku/${hakuOid}/hakukohde/${hk.oid}/${activeHakukohdeTab.route}`}
-          tabIndex={0}
-        >
-          <OphTypography
-            title={hk.organisaatioOid}
-            variant="label"
-            color="inherit"
+      <StyledList tabIndex={0} aria-label={t('hakukohde.navigaatio')}>
+        {results?.map((hk: Hakukohde) => (
+          <StyledItem
+            key={hk.oid}
+            className={selectedHakukohdeOid === hk.oid ? SELECTED_CLASS : ''}
+            href={`/haku/${hakuOid}/hakukohde/${hk.oid}/${activeHakukohdeTab.route}`}
+            tabIndex={0}
           >
-            {hk.jarjestyspaikkaHierarkiaNimi
-              ? translateEntity(hk.jarjestyspaikkaHierarkiaNimi)
-              : ''}
-          </OphTypography>
-          <OphTypography title={hk.oid} color="inherit">
-            {translateEntity(hk.nimi)}
-          </OphTypography>
-        </StyledItem>
-      ))}
-    </StyledList>
+            <OphTypography
+              title={hk.organisaatioOid}
+              variant="label"
+              color="inherit"
+            >
+              {hk.jarjestyspaikkaHierarkiaNimi
+                ? translateEntity(hk.jarjestyspaikkaHierarkiaNimi)
+                : ''}
+            </OphTypography>
+            <OphTypography title={hk.oid} color="inherit">
+              {translateEntity(hk.nimi)}
+            </OphTypography>
+          </StyledItem>
+        ))}
+      </StyledList>
+    </>
   );
 };
 
