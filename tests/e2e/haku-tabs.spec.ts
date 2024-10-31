bfe64b0859c3d0495a5f86ce99e802ed35e2d063
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-10-31 09:17:46 +0200

    OK-669: Lisätty Playwright-testit haku-välilehdille

---
+++ tests/e2e/haku-tabs.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+import {
+  expectAllSpinnersHidden,
+  expectPageAccessibilityOk,
+  getHakuNaviLinks,
+} from './playwright-utils';
+
+type Tab = {
+  title: string;
+  route: string;
+};
+
+const TABS_TO_TEST: Tab[] = [
+  {
+    title: 'Hakukohteittain',
+    route: 'hakukohde',
+  },
+  {
+    title: 'Henkilöittäin',
+    route: 'henkilo',
+  },
+  {
+    title: 'Valintaryhmittäin',
+    route: 'valintaryhma',
+  },
+  {
+    title: 'Yhteisvalinnan hallinta',
+    route: 'yhteisvalinnan-hallinta',
+  },
+] as const;
+
+for (const tab of TABS_TO_TEST) {
+  test(`Navigates to ${tab.title}`, async ({ page }) => {
+    await page.goto(
+      '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102',
+    );
+
+    await getHakuNaviLinks(page).filter({ hasText: tab.title }).click();
+    await page.waitForURL(
+      (url) =>
+        url.pathname ===
+        `/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102/${tab.route}`,
+    );
+    await expect(page.getByAltText('virhe')).toBeHidden();
+  });
+
+  test(`${tab.title} page accessibility`, async ({ page }) => {
+    await page.goto(
+      `/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102/${tab.route}`,
+    );
+    await expectAllSpinnersHidden(page);
+    await expectPageAccessibilityOk(page);
+  });
+}
