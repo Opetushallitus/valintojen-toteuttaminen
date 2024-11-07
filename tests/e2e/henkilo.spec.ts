ae06a173b2d8428ddffe9654eb0007a359b7e2b4
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-11-07 13:34:55 +0200

    OK-670: Parannettu henkilö-suodatuksen Playwright-testiä

--- tests/e2e/henkilo.spec.ts
+++ tests/e2e/henkilo.spec.ts
@@ -3,6 +3,7 @@ import {
   expectAllSpinnersHidden,
   expectPageAccessibilityOk,
 } from './playwright-utils';
+import HAKENEET from './fixtures/hakeneet.json';
 
 test('Henkiloittain page accessibility', async ({ page }) => {
   await page.goto(
@@ -17,6 +18,34 @@ test('Henkilö-search and navigation works', async ({ page }) => {
     '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102/henkilo',
   );
 
+  await page.route('**/lomake-editori/api/external/valinta-ui*', (route) => {
+    const url = route.request().url();
+    switch (true) {
+      case url.includes('name=Ruhtinas'):
+        return route.fulfill({
+          json: HAKENEET.filter((h) => h.etunimet === 'Ruhtinas'),
+        });
+      case url.includes('henkilotunnus=210988-9151'):
+        return route.fulfill({
+          json: HAKENEET.filter((h) => h.henkilotunnus === '210988-9151'),
+        });
+      case url.includes('hakemusOids=1.2.246.562.11.00000000000001793706'):
+        return route.fulfill({
+          json: HAKENEET.filter(
+            (h) => h.oid === '1.2.246.562.11.00000000000001793706',
+          ),
+        });
+      case url.includes('henkiloOid=1.2.246.562.24.30476885816'):
+        return route.fulfill({
+          json: HAKENEET.filter(
+            (h) => h.personOid === '1.2.246.562.24.30476885816',
+          ),
+        });
+      default:
+        return route.continue();
+    }
+  });
+
   const henkiloSearchInput = page.getByRole('textbox', {
     name: 'Hae henkilöitä',
   });
@@ -25,32 +54,42 @@ test('Henkilö-search and navigation works', async ({ page }) => {
     name: 'Henkilövalitsin',
   });
 
+  const henkiloLinks = henkiloNavigation.getByRole('link');
+
   await expect(henkiloNavigation).toBeHidden();
+
+  await Promise.all([
+    henkiloSearchInput.fill('210988-9151'),
+    page.waitForRequest((request) =>
+      request.url().includes('henkilotunnus=210988-9151'),
+    ),
+    expect(henkiloLinks).toHaveCount(1),
+    expect(henkiloLinks.first()).toContainText('Purukumi Puru'),
+  ]);
+
+  await Promise.all([
+    henkiloSearchInput.fill('1.2.246.562.11.00000000000001793706'),
+    page.waitForRequest((request) =>
+      request.url().includes('hakemusOids=1.2.246.562.11.00000000000001793706'),
+    ),
+    expect(henkiloLinks).toHaveCount(1),
+    expect(henkiloLinks.first()).toContainText('Dacula Kreivi'),
+  ]);
+
+  await Promise.all([
+    henkiloSearchInput.fill('1.2.246.562.24.30476885816'),
+    page.waitForRequest((request) =>
+      request.url().includes('henkiloOid=1.2.246.562.24.30476885816'),
+    ),
+    expect(henkiloLinks).toHaveCount(1),
+    expect(henkiloLinks.first()).toContainText('Hui Haamu'),
+  ]);
 
   await Promise.all([
     henkiloSearchInput.fill('Ruhtinas'),
     page.waitForRequest((request) => request.url().includes('name=Ruhtinas')),
-  ]);
-
-  await Promise.all([
-    henkiloSearchInput.fill('123456-123X'),
-    page.waitForRequest((request) =>
-      request.url().includes('henkilotunnus=123456-123X'),
-    ),
-  ]);
-
-  await Promise.all([
-    henkiloSearchInput.fill('1.2.246.562.11.00000000000001796027'),
-    page.waitForRequest((request) =>
-      request.url().includes('hakemusOids=1.2.246.562.11.00000000000001796027'),
-    ),
-  ]);
-
-  await Promise.all([
-    henkiloSearchInput.fill('1.2.246.562.24.2633265254'),
-    page.waitForRequest((request) =>
-      request.url().includes('henkiloOid=1.2.246.562.24.2633265254'),
-    ),
+    expect(henkiloLinks).toHaveCount(1),
+    expect(henkiloLinks.first()).toContainText('Nukettaja Ruhtinas'),
   ]);
 
   await henkiloNavigation
