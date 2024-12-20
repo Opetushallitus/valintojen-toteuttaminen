dc70f4b74ec8eb3b3809229e5aee9651f3c3e4e7
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-12-20 09:30:16 +0200

    OK-674: Lisätty henkilö-näkymään valintalaskennan käynnistämiseen ja tilan näyttämiseen liittyviä komponentteja

--- src/app/haku/[oid]/henkilo/[hakemusOid]/page.tsx
+++ src/app/haku/[oid]/henkilo/[hakemusOid]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useTranslations } from '@/app/hooks/useTranslations';
 import { buildLinkToApplication } from '@/app/lib/ataru';
-import { Stack, Typography } from '@mui/material';
+import { LinearProgress, Stack, styled, Typography } from '@mui/material';
 import { getHenkiloTitle } from '@/app/lib/henkilo-utils';
 import { LabeledInfoItem } from '@/app/components/labeled-info-item';
 import { ExternalLink } from '@/app/components/external-link';
@@ -10,8 +10,81 @@ import { QuerySuspenseBoundary } from '@/app/components/query-suspense-boundary'
 import { FullClientSpinner } from '@/app/components/client-spinner';
 import { HakutoiveetTable } from './components/hakutoiveet-table';
 import { useHenkiloPageData } from './hooks/useHenkiloPageData';
-import { use } from 'react';
+import { use, useState } from 'react';
 import { HenkilonPistesyotto } from './components/henkilon-pistesyotto';
+import {
+  OphButton,
+  ophColors,
+  OphTypography,
+} from '@opetushallitus/oph-design-system';
+import { withDefaultProps } from '@/app/lib/mui-utils';
+
+const PROGRESSBAR_HEIGHT = '42px';
+
+const ProgressBar = ({ value }: { value: number }) => {
+  return (
+    <LinearProgress
+      value={value}
+      variant="determinate"
+      sx={{
+        display: 'block',
+        height: PROGRESSBAR_HEIGHT,
+        maxWidth: '700px',
+        borderRadius: '2px',
+        border: `1px solid ${ophColors.grey300}`,
+        backgroundColor: ophColors.white,
+        '& .MuiLinearProgress-barColorPrimary': {
+          backgroundColor: ophColors.cyan1,
+        },
+      }}
+    />
+  );
+};
+
+const LaskentaButton = withDefaultProps(
+  styled(OphButton)({
+    alignSelf: 'flex-start',
+  }),
+  {
+    variant: 'contained',
+  },
+);
+
+const Valintalaskenta = () => {
+  const { t } = useTranslations();
+
+  const [laskentaKaynnissa, setLaskentaKaynnissa] = useState(false);
+
+  return (
+    <Stack spacing={2}>
+      {laskentaKaynnissa ? (
+        <>
+          <OphTypography variant="h4">
+            {t('henkilo.valintalaskenta')}
+          </OphTypography>
+          <ProgressBar value={10} />
+          <LaskentaButton
+            onClick={() => {
+              setLaskentaKaynnissa(false);
+            }}
+          >
+            {t('henkilo.keskeyta-valintalaskenta')}
+          </LaskentaButton>
+        </>
+      ) : (
+        <>
+          <LaskentaButton
+            onClick={() => {
+              setLaskentaKaynnissa(true);
+            }}
+          >
+            {t('henkilo.suorita-valintalaskenta')}
+          </LaskentaButton>
+        </>
+      )}
+    </Stack>
+  );
+};
 
 const HenkiloContent = ({
   hakuOid,
@@ -30,6 +103,7 @@ const HenkiloContent = ({
   return (
     <>
       <Typography variant="h2">{getHenkiloTitle(hakija)}</Typography>
+      <Valintalaskenta />
       <Stack direction="row" spacing="4vw">
         <LabeledInfoItem
           label={t('henkilo.hakemus-oid')}
