2403fdc4a2b481d1beb29eb9caad5b57c7e14253
Risto Salama <risto.salama@gofore.com>
2025-01-14 14:09:01 +0200

    Calendar styles

--- src/app/haku/[oid]/hakukohde/[hakukohde]/sijoittelun-tulokset/components/letter-template-modal.tsx
+++ src/app/haku/[oid]/hakukohde/[hakukohde]/sijoittelun-tulokset/components/letter-template-modal.tsx
@@ -11,7 +11,11 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { forwardRef, ReactNode, Ref, useState } from 'react';
 import { OphFormControl } from '@/app/components/form/oph-form-control';
-import { CalendarTodayOutlined } from '@mui/icons-material';
+import {
+  CalendarTodayOutlined,
+  KeyboardArrowRight,
+  KeyboardArrowLeft,
+} from '@mui/icons-material';
 import { capitalize } from 'remeda';
 
 const CALENDAR_CLASSNAME = 'oph-calendar';
@@ -22,16 +26,35 @@ const CalendarStyles = styled(Box)(({ theme }) => ({
       '&__header': {
         backgroundColor: ophColors.white,
         borderBottom: 'none',
+        paddingTop: '0.7rem',
         'h2.react-datepicker__current-month, .react-datepicker-time__header': {
           ...theme.typography.label,
         },
       },
-      '&__day--selected, &__day--keyboard-selected, &__day:hover': {
-        backgroundColor: ophColors.blue2,
-        borderRadius: 45,
-        color: ophColors.white,
+      '&__day': {
+        fontSize: 'inherit',
+        width: '2rem',
+        height: '2rem',
+        lineHeight: '2rem',
+        verticalAlign: 'bottom',
+      },
+      '&__day--selected, &__day--keyboard-selected, &__day:hover:not(&__day--disabled)':
+        {
+          backgroundColor: ophColors.blue2,
+          borderRadius: 45,
+          color: ophColors.white,
+        },
+      '&__day-name': {
+        color: ophColors.grey400,
+        width: '2rem',
+        marginTop: '0.5rem',
       },
     },
+    '.react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item':
+      {
+        height: 'unset',
+        marginBottom: '0.2rem',
+      },
     '.react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item--selected':
       {
         backgroundColor: ophColors.blue2,
@@ -44,6 +67,21 @@ const CalendarStyles = styled(Box)(({ theme }) => ({
         backgroundColor: ophColors.blue2,
         color: ophColors.white,
       },
+    '&__navigation': {
+      marginTop: '0.25rem',
+      marginLeft: '0.7rem',
+      marginRight: '0.7rem',
+      '&-icon': {
+        border: '1px solid',
+        borderColor: ophColors.grey300,
+        borderRadius: 45,
+        color: ophColors.grey300,
+        '&:hover': {
+          borderColor: ophColors.blue2,
+          color: ophColors.blue2,
+        },
+      },
+    },
   },
   '.react-datepicker__triangle': {
     display: 'none',
@@ -65,6 +103,7 @@ const TemplateModalContent = () => {
   const CustomInput = forwardRef<Ref<HTMLInputElement>, CalendarInputProps>(
     (props, ref) => (
       <StyledOphInput
+        placeholder={t('kalenteri.syote-vihje')}
         value={props.value}
         onClick={props.onClick}
         ref={ref as Ref<HTMLInputElement>}
@@ -91,17 +130,51 @@ const TemplateModalContent = () => {
             customInput={<CustomInput />}
             calendarClassName={CALENDAR_CLASSNAME}
             showTimeSelect
-            timeCaption={t('yleinen.aika')}
-            dateFormat="dd.MM.yyyy hh:mm"
+            timeCaption={t('kalenteri.aika')}
+            dateFormat="dd.MM.yyyy HH:mm"
             formatWeekDay={(wd) => capitalize(wd).substring(0, 2)}
             locale={language}
-            placeholderText="pp.kk.vvvv hh:mm"
+            placeholderText={t('kalenteri.syote-vihje')}
             popperContainer={({ children }: { children?: ReactNode }) => (
               <Box sx={{ position: 'fixed', zIndex: 9999, width: '100%' }}>
                 {children}
               </Box>
             )}
             popperPlacement="bottom-start"
+            renderCustomHeader={({
+              monthDate,
+              decreaseMonth,
+              increaseMonth,
+            }) => (
+              <div>
+                <button
+                  aria-label="Previous Month"
+                  className={
+                    'react-datepicker__navigation react-datepicker__navigation--previous oph-calendar__navigation'
+                  }
+                  onClick={decreaseMonth}
+                >
+                  <KeyboardArrowLeft className="oph-calendar__navigation-icon" />
+                </button>
+                <span className="react-datepicker__current-month">
+                  {capitalize(
+                    monthDate.toLocaleString(language, {
+                      month: 'long',
+                      year: 'numeric',
+                    }),
+                  )}
+                </span>
+                <button
+                  aria-label="Next Month"
+                  className={
+                    'react-datepicker__navigation react-datepicker__navigation--next oph-calendar__navigation'
+                  }
+                  onClick={increaseMonth}
+                >
+                  <KeyboardArrowRight className="oph-calendar__navigation-icon" />
+                </button>
+              </div>
+            )}
           />
         )}
       />
