b7b308e2493be152715f6eeac92ffcae07eb8f60
Risto Salama <risto.salama@gofore.com>
2025-01-22 11:00:19 +0200

    Some typings

--- src/app/haku/[oid]/hakukohde/[hakukohde]/sijoittelun-tulokset/components/editor-component.tsx
+++ src/app/haku/[oid]/hakukohde/[hakukohde]/sijoittelun-tulokset/components/editor-component.tsx
@@ -3,14 +3,18 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
 // Editor is an uncontrolled React component
-const Editor = forwardRef(
-  ({ defaultValue, setContentChanged }, ref) => {
-    const containerRef = useRef(null);
-    const defaultValueRef = useRef(defaultValue);
-    const contentChangedRef = useRef(setContentChanged);
+const Editor = forwardRef<
+  Quill,
+  { defaultValue: string; setContentChanged: (val: string) => void }
+>(({ defaultValue, setContentChanged }, ref) => {
+  const containerRef = useRef(null);
+  const defaultValueRef = useRef(defaultValue);
+  const contentChangedRef = useRef(setContentChanged);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (containerRef.current) {
       const container = containerRef.current;
+
       const editorContainer = container.appendChild(
         container.ownerDocument.createElement('div'),
       );
@@ -25,19 +29,18 @@ const Editor = forwardRef(
         quill.setContents(delta);
       }
 
-      quill.on(Quill.events.TEXT_CHANGE, (...args) => {
+      quill.on(Quill.events.TEXT_CHANGE, () => {
         contentChangedRef?.current?.(quill.getSemanticHTML());
       });
-
       return () => {
         ref.current = null;
         container.innerHTML = '';
       };
-    }, [ref]);
+    }
+  }, [ref]);
 
-    return <div id="oph-editor" ref={containerRef}></div>;
-  },
-);
+  return <div id="oph-editor" ref={containerRef}></div>;
+});
 
 Editor.displayName = 'Editor';
 
@@ -46,21 +49,25 @@ export type EditorProps = {
   setContentChanged: (value: string) => void;
 };
 
-export const EditorComponent = (
-  {editorContent, setContentChanged}: EditorProps
-) => {
-
-  const quillRef = useRef({defaultValue: editorContent, setContentChanged});
+export const EditorComponent = ({
+  editorContent,
+  setContentChanged,
+}: EditorProps) => {
+  const quillRef = useRef({ defaultValue: editorContent, setContentChanged });
 
   useEffect(() => {
     const quill = quillRef.current;
     if (quill instanceof Quill) {
-      const delta = quill.clipboard.convert({html: editorContent});
+      const delta = quill.clipboard.convert({ html: editorContent });
       quill.setContents(delta);
     }
-  }, [editorContent])
+  }, [editorContent]);
 
   return (
-    <Editor ref={quillRef} defaultValue={editorContent} setContentChanged={setContentChanged}/>);
+    <Editor
+      ref={quillRef}
+      defaultValue={editorContent}
+      setContentChanged={setContentChanged}
+    />
+  );
 };
-
