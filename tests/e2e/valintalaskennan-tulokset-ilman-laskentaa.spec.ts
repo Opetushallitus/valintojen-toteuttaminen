e067132d4e03e97d0a49a8db20cd1201c527c9c6
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-01-24 16:49:06 +0200

    OK-746: Lisätty Playwright-savutestejä valintalaskennan tuloksille ilman laskentaa

---
+++ tests/e2e/valintalaskennan-tulokset-ilman-laskentaa.spec.ts
@@ -0,0 +1,86 @@
+import { test, expect } from '@playwright/test';
+import {
+  checkRow,
+  expectAllSpinnersHidden,
+  expectPageAccessibilityOk,
+} from './playwright-utils';
+import VALINNANVAIHE_TULOKSET_ILMAN_LASKENTAA from './fixtures/valinnanvaihe-tulokset-ilman-laskentaa.json';
+import VALINNANVAIHEET_ILMAN_LASKENTAA from './fixtures/valinnanvaiheet-ilman-laskentaa.json';
+
+const JONOSIJA_TABLE_HEADINGS = [
+  'Jonosija',
+  'Hakija',
+  'Valintatieto',
+  'Kuvaus suomeksi',
+  'Kuvaus ruotsiksi',
+  'Kuvaus englanniksi',
+];
+
+test.beforeEach(async ({ page }) => {
+  await page.route(
+    '**/valintalaskenta-laskenta-service/resources/hakukohde/**',
+    async (route) =>
+      await route.fulfill({ json: VALINNANVAIHE_TULOKSET_ILMAN_LASKENTAA }),
+  );
+  await page.route(
+    /valintaperusteet-service\/resources\/hakukohde\/\S+\/ilmanlaskentaa/,
+    async (route) =>
+      await route.fulfill({ json: VALINNANVAIHEET_ILMAN_LASKENTAA }),
+  );
+});
+
+test('valintalaskennan tulokset ilman laskentaa accessibility', async ({
+  page,
+}) => {
+  await page.goto(
+    '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102/hakukohde/1.2.246.562.20.00000000000000045105/valintalaskennan-tulokset',
+  );
+  await expectAllSpinnersHidden(page);
+  await page.locator('tbody tr').nth(1).hover();
+  await expectPageAccessibilityOk(page);
+});
+
+test('displays valintalaskennan tulokset', async ({ page }) => {
+  await page.goto(
+    '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000045102/hakukohde/1.2.246.562.20.00000000000000045105/valintalaskennan-tulokset',
+  );
+  await expectAllSpinnersHidden(page);
+  await expect(
+    page.getByRole('heading', {
+      level: 1,
+      name: '> Tampere University Separate Admission/ Finnish MAOL Competition Route 2024',
+    }),
+  ).toBeVisible();
+
+  await expect(page.getByRole('heading', { level: 2 })).toHaveText(
+    'Tampereen yliopisto, Rakennetun ympäristön tiedekunta' +
+      'Finnish MAOL competition route, Technology, Sustainable Urban Development, Bachelor and Master of Science (Technology) (3 + 2 yrs)',
+  );
+
+  await expect(
+    page.getByRole('button', { name: 'Vie kaikki taulukkolaskentaan' }),
+  ).toBeVisible();
+
+  await expect(page.getByRole('heading', { level: 3 })).toHaveText(
+    'Valinnanvaiheet, joissa ei käytetä laskentaa',
+  );
+
+  const jono1HeadingText =
+    'Varsinainen valinta: Tutkintoon valmentava koulutus';
+
+  await expect(
+    page.getByRole('heading', {
+      level: 4,
+      name: jono1HeadingText,
+    }),
+  ).toBeVisible();
+
+  const jono1Content = page.getByRole('region', { name: jono1HeadingText });
+
+  const jono1HeadingRow = jono1Content.locator('thead tr');
+  await checkRow(jono1HeadingRow, JONOSIJA_TABLE_HEADINGS, 'th');
+
+  const jono1Rows = jono1Content.locator('tbody tr');
+
+  await expect(jono1Rows).toHaveCount(4);
+});
