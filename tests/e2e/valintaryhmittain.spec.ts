0598d6eae3607df8810e3cb2219a4d064c82a534
Risto Salama <risto.salama@gofore.com>
2025-02-25 09:06:49 +0200

    Testaa valintaryhmään navigointi

--- tests/e2e/valintaryhmittain.spec.ts
+++ tests/e2e/valintaryhmittain.spec.ts
@@ -81,3 +81,105 @@ test.describe('Valintaryhmillä hakeminen', () => {
     ).toBeVisible();
   });
 });
+
+test.describe('Valintaryhmään navigoiminen', () => {
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.route(
+      '*/**/kouta-internal/hakukohde/search?all=false&haku=1.2.246.562.29.00000000000000017683*',
+      async (route) => {
+        const hakukohteet = [
+          {
+            oid: 'hakukohde-1',
+            hakuOid: '1.2.246.562.29.00000000000000017683',
+            nimi: { fi: 'Hiekkalinnan rakentajat' },
+            organisaatioOid: 'organisaatio-1',
+            organisaatioNimi: { fi: 'Hiekkalaatikko' },
+            jarjestyspaikkaHierarkiaNimi: { fi: 'Hiekkasärkät' },
+            voikoHakukohteessaOllaHarkinnanvaraisestiHakeneita: false,
+            opetuskieliKoodiUrit: ['fi'],
+            tarjoaja: 'tarjoaja-1',
+          },
+          {
+            oid: 'hakukohde-2',
+            hakuOid: '1.2.246.562.29.00000000000000017683',
+            nimi: { fi: 'Pihakeinun pystyttäjät' },
+            organisaatioOid: 'organisaatio-2',
+            organisaatioNimi: { fi: 'Pihakeinu' },
+            jarjestyspaikkaHierarkiaNimi: { fi: 'Sibeliuksen puisto' },
+            voikoHakukohteessaOllaHarkinnanvaraisestiHakeneita: false,
+            opetuskieliKoodiUrit: ['fi'],
+            tarjoaja: 'tarjoaja-2',
+          },
+          {
+            oid: 'hakukohde-3',
+            hakuOid: '1.2.246.562.29.00000000000000017683',
+            nimi: { fi: 'Liukumäen testaajat' },
+            organisaatioOid: 'organisaatio-2',
+            organisaatioNimi: { fi: 'Liukumäki' },
+            jarjestyspaikkaHierarkiaNimi: { fi: 'Sibeliuksen puisto' },
+            voikoHakukohteessaOllaHarkinnanvaraisestiHakeneita: false,
+            opetuskieliKoodiUrit: ['fi'],
+            tarjoaja: 'tarjoaja-2',
+          },
+        ];
+        await route.fulfill({ json: hakukohteet });
+      },
+    );
+  });
+
+  test('klikkaamalla valintaryhmää', async () => {
+    await page.goto(
+      '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000017683/valintaryhma',
+    );
+    await page.getByRole('link', { name: 'Pääsykoe' }).click();
+    await expect(page.getByText('Valitse valintaryhmä')).toBeHidden();
+    await expect(
+      page.getByRole('cell', { name: 'Hiekkasärkät, Hiekkalinnan' }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole('cell', {
+        name: 'Sibeliuksen puisto, Pihakeinun pystyttäjät',
+      }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole('cell', {
+        name: 'Sibeliuksen puisto, Liukumäen testaajat',
+      }),
+    ).toBeHidden();
+  });
+
+  test('klikkaamalla valintaryhmää jolla ei hakukohdeviitteitä', async () => {
+    await page.goto(
+      '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000017683/valintaryhma',
+    );
+    await page.getByRole('link', { name: 'Peruskoulupohjaiset' }).click();
+    await expect(page.getByText('Valitse valintaryhmä')).toBeHidden();
+    await expect(
+      page.getByText('Valintaryhmässä ei ole hakukohteita'),
+    ).toBeVisible();
+  });
+
+  test('suoralla linkillä', async () => {
+    await page.goto(
+      '/valintojen-toteuttaminen/haku/1.2.246.562.29.00000000000000017683/valintaryhma/2234567-3234567',
+    );
+    await page.getByRole('link', { name: 'Pääsykoe' }).click();
+    await expect(page.getByText('Valitse valintaryhmä')).toBeHidden();
+    await expect(
+      page.getByRole('cell', { name: 'Hiekkasärkät, Hiekkalinnan' }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole('cell', {
+        name: 'Sibeliuksen puisto, Pihakeinun pystyttäjät',
+      }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole('cell', {
+        name: 'Sibeliuksen puisto, Liukumäen testaajat',
+      }),
+    ).toBeHidden();
+  });
+});
