3a5381cfc2f2d9a8791f4b6f399cf655b9715d7f
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-03-04 15:00:07 +0200

    Poistettu tarpeettomaksi jäänyt useXstateMachine

--- src/lib/xstate-utils.ts
+++ src/lib/xstate-utils.ts
@@ -1,19 +1,8 @@
-import { useMachine } from '@xstate/react';
-
 import { createBrowserInspector } from '@statelyai/inspect';
 import { isServer } from '@tanstack/react-query';
 import { xstateInspect } from './configuration';
-
-type UseMachineParams = Parameters<typeof useMachine>;
 
 export const inspect =
   xstateInspect && isServer
     ? undefined
     : (createBrowserInspector()?.inspect ?? undefined);
-
-export const useXstateMachine = (
-  machine: UseMachineParams[0],
-  options: UseMachineParams[1] = {},
-) => {
-  return useMachine(machine, { inspect, ...options });
-};
