4092a88decd7cbaeec807bd142ff8cc272f2b30b
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-03-17 08:16:28 +0200

    OK-679: Paranneltu valintaperusteiden apufunktioita
    - checkIsValintalaskentaUsed-funktiossa tehdään tarkistukset vain aktiivisille valinnanvaiheille.
    - Lisätty kommentteja

--- src/lib/valintaperusteet/valintaperusteet-utils.ts
+++ src/lib/valintaperusteet/valintaperusteet-utils.ts
@@ -1,27 +1,34 @@
-import { isEmpty, last } from 'remeda';
+import { isEmpty, last, prop } from 'remeda';
 import { Valinnanvaihe } from './valintaperusteet-types';
 
+/**
+ * Tarkistaa, onko valintalaskenta käytössä Hakukohteen valinnanvaiheissa tai valintatapajonossa.
+ * @param valinnanvaiheet Hakukohteen valinnanvaiheet valintaperusteista järjestyksessä.
+ * @param jonoOid Valintatapajonon tunniste. Jos jonon id:tä ei ole annettu, tarkistetaan onko laskenta käytössä hakukohteessa.
+ */
 export const checkIsValintalaskentaUsed = (
   valinnanvaiheet: Array<Valinnanvaihe>,
   jonoOid?: string,
 ) => {
+  const aktiivisetVaiheet = valinnanvaiheet.filter(prop('aktiivinen'));
   if (jonoOid) {
-    const jono = valinnanvaiheet
-      .filter((v) => v.aktiivinen)
+    const jono = aktiivisetVaiheet
       .flatMap((v) => v.jonot)
       .find((j) => j.oid === jonoOid);
-    return jono?.kaytetaanValintalaskentaa || false;
+    return Boolean(jono?.kaytetaanValintalaskentaa);
   } else {
-    // valintaperusteet palauttaa valinnanvaiheet järjestettynä, ja viimeinen valinnanvaihe määrittää onko laskenta käytössä
-    const lastValinnanvaihe = last(valinnanvaiheet);
+    // Muiden valinnanvaiheiden tulokset jätetään huomiotta, jos viimeisessä aktiivisessa valinnanvaiheessa ei ole laskentaa
+    const lastValinnanvaihe = last(aktiivisetVaiheet);
     return Boolean(
-      lastValinnanvaihe?.aktiivinen &&
-        !isEmpty(lastValinnanvaihe.jonot) &&
+      !isEmpty(lastValinnanvaihe?.jonot ?? []) &&
         lastValinnanvaihe?.jonot.some((jono) => jono.kaytetaanValintalaskentaa),
     );
   }
 };
 
+/** Palauttaa valinnanvaiheet ja niiden jonot, jotka ei käytä valintalaskentaa.
+ * @param valinnanvaiheet Hakukohteen valinnanvaiheet (valintaperusteista).
+ */
 export const selectLaskennattomatValinnanvaiheet = (
   valinnanvaiheet: Array<Valinnanvaihe>,
 ) => {
