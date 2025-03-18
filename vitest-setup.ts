7d2f613c234c93c6da30e6190044993293547c2d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-03-18 15:45:37 +0200

    Mockataan useTranslations identiteettifunktiolla kaikissa Vitest-testeissä

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
+
+vi.mock('@/lib/localization/useTranslations', () => ({
+  useTranslations: () => {
+    return { t: (x: string) => x };
+  },
+}));
