76cb0b12e1941129412cadd41db1503e6b4b0853
Risto Salama <risto.salama@gofore.com>
2025-03-19 18:31:42 +0200

    Add blank sijoittelu elements

--- src/app/haku/[oid]/yhteisvalinnan-hallinta/page.tsx
+++ src/app/haku/[oid]/yhteisvalinnan-hallinta/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { useTranslations } from '@/lib/localization/useTranslations';
 import { Box } from '@mui/material';
+import { SijoitteluContainer } from './sijoittelu/components/sijoittelu-container';
 
 export default function YhteisvalinnanHallintaPage() {
   const { t } = useTranslations();
   return (
     <Box sx={{ padding: 4 }}>
       TODO: {t('haku-tabs.yhteisvalinnan-hallinta')}
+      <SijoitteluContainer />
     </Box>
   );
 }
