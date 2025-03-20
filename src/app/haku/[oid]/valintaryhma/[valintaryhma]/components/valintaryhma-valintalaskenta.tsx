1caec5c9a1da14c5d9511c7c3f78fa4701385141
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-03-20 10:31:58 +0200

    OK-891: Refaktoroitu valintalaskennan tilan ja etenemisen esittäminen yhteiseen komponenttiin

--- src/app/haku/[oid]/valintaryhma/[valintaryhma]/components/valintaryhma-valintalaskenta.tsx
+++ src/app/haku/[oid]/valintaryhma/[valintaryhma]/components/valintaryhma-valintalaskenta.tsx
@@ -1,61 +1,21 @@
 'use client';
 
 import { useTranslations } from '@/lib/localization/useTranslations';
-import { CircularProgress, Divider, Stack, Typography } from '@mui/material';
-import { OphTypography } from '@opetushallitus/oph-design-system';
+import { Divider, Stack } from '@mui/material';
 import {
   LaskentaActorRef,
   LaskentaEvent,
   LaskentaEventType,
   LaskentaMachineSnapshot,
   LaskentaState,
-  useLaskentaError,
 } from '@/lib/state/laskenta-state';
 import { Hakukohde } from '@/lib/kouta/kouta-types';
 import { ErrorAlert } from '@/components/error-alert';
 import { useSelector } from '@xstate/react';
 import { ConfirmationModal } from '@/components/modals/confirmation-modal';
-import { getLaskentaStatusText } from '@/lib/valintalaskenta/valintalaskenta-utils';
 import { SuorittamattomatHakukohteet } from '@/components/suorittamattomat-hakukohteet';
 import { LaskentaStateButton } from '@/components/laskenta-state-button';
-
-const LaskentaResult = ({ actorRef }: { actorRef: LaskentaActorRef }) => {
-  const { t } = useTranslations();
-
-  const laskentaError = useLaskentaError(actorRef);
-
-  const state = useSelector(actorRef, (s) => s);
-  const seurantaTiedot = useSelector(actorRef, (s) => s.context.seurantaTiedot);
-
-  switch (true) {
-    case state.matches({ [LaskentaState.IDLE]: LaskentaState.ERROR }):
-      return (
-        <ErrorAlert
-          title={t('valintalaskenta.valintalaskenta-epaonnistui')}
-          message={laskentaError}
-        />
-      );
-    case state.hasTag('started') || state.hasTag('completed'):
-      return (
-        <>
-          <OphTypography variant="h4">
-            {t('valintalaskenta.valintalaskenta')}
-          </OphTypography>
-          {state.matches(LaskentaState.PROCESSING) && (
-            <CircularProgress aria-label={t('valinnanhallinta.lasketaan')} />
-          )}
-          <Typography>
-            {getLaskentaStatusText(state, seurantaTiedot, t)}
-            {seurantaTiedot &&
-              `${t('valintalaskenta.hakukohteita-valmiina')} ${seurantaTiedot.hakukohteitaValmiina}/${seurantaTiedot.hakukohteitaYhteensa}. ` +
-                `${t('valintalaskenta.suorittamattomia-hakukohteita')} ${seurantaTiedot?.hakukohteitaKeskeytetty ?? 0}.`}
-          </Typography>
-        </>
-      );
-    default:
-      return null;
-  }
-};
+import { ValintalaskentaStatus } from '@/components/ValintalaskentaStatus';
 
 export const ValintaryhmanValintalaskenta = ({
   hakukohteet,
@@ -82,7 +42,10 @@ export const ValintaryhmanValintalaskenta = ({
         onConfirm={() => send({ type: LaskentaEventType.CONFIRM })}
         onCancel={() => send({ type: LaskentaEventType.CANCEL })}
       />
-      <LaskentaResult actorRef={actorRef} />
+      <ValintalaskentaStatus
+        laskentaActorRef={actorRef}
+        progressType="spinner"
+      />
       <LaskentaStateButton state={state} send={send} />
       {state.hasTag('completed') && summaryIlmoitus && (
         <ErrorAlert
