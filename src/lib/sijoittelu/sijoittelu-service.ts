9f291cf836f40fdd50f2df18257e46347ec5cc24
Risto Salama <risto.salama@gofore.com>
2025-03-21 13:11:32 +0200

    Start sijoittelu and indicate it is in progress

--- src/lib/sijoittelu/sijoittelu-service.ts
+++ src/lib/sijoittelu/sijoittelu-service.ts
@@ -2,7 +2,7 @@ import { configuration } from '../configuration';
 import { client } from '../http-client';
 
 export async function kaynnistaSijoittelu(hakuOid: string) {
-  await client.post(configuration.kaynnistaSijoittelu, { hakuOid });
+  await client.post(configuration.kaynnistaSijoittelu({ hakuOid }), {});
 }
 
 export async function sijoittelunStatus(
@@ -11,6 +11,5 @@ export async function sijoittelunStatus(
   const response = await client.get<{ valmis: boolean; ohitettu: boolean }>(
     configuration.sijoittelunStatus({ hakuOid }),
   );
-  console.log(response.data);
   return response.data;
 }
