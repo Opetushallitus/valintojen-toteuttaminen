d3d693a90e30d4ace361b593c9ca3a64045ec01c
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-04-11 15:27:42 +0300

    Korjattu Playwright testien ajaminen dockerissa MacOS:Ssä

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `https://localhost:3404${nextConfig.basePath}`,
+    baseURL: `https://${process.env.DOCKER ? 'host.docker.internal' : 'localhost'}:3404${nextConfig.basePath}`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     ignoreHTTPSErrors: true,
--- scripts/run-playwright-tests-in-docker.sh
+++ scripts/run-playwright-tests-in-docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 PLAYWRIGHT_VERSION=$(node -e "console.log(require('./package-lock.json').packages['node_modules/@playwright/test'].version)")
-docker run --mount type=bind,source=$PWD,target=/app --user "$(id -u):$(id -g)" -w /app --ipc=host --net=host mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION npx playwright test $@
+
+docker run --mount type=bind,source=$PWD,target=/app --user "$(id -u):$(id -g)" -w /app/$SUBDIR \
+--add-host=host.docker.internal:host-gateway -e DOCKER=1 \
+mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION \
+npx playwright test
