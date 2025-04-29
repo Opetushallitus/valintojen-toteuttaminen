d78e62a356aadf32c529cd33bf02d8c029a98043
Risto Salama <risto.salama@gofore.com>
2025-04-29 08:47:51 +0300

    Use custom cachepolicy for server from parameter store

--- cdk/lib/sovellus-stack.ts
+++ cdk/lib/sovellus-stack.ts
@@ -9,6 +9,7 @@ import {
   OptionalFunctionProps,
 } from 'cdk-nextjs-standalone';
 import { CachePolicy, PriceClass } from 'aws-cdk-lib/aws-cloudfront';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 
 type EnvironmentName = 'untuva' | 'hahtuva' | 'pallero';
 
@@ -71,6 +72,13 @@ export class SovellusStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
+    const OPEN_NEXT_SERVER_CACHE_POLICY_ID =
+      StringParameter.fromStringParameterAttributes(
+        this,
+        'serverCachePolicyId',
+        { parameterName: '/untuva/NextJs/serverCachePolicyId' },
+      ).stringValue;
+
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
       this,
       'PublicHostedZone',
@@ -107,7 +115,7 @@ export class SovellusStack extends cdk.Stack {
       overrides: {
         nextjsDistribution: {
           serverBehaviorOptions: {
-            cachePolicy: CachePolicy.CACHING_OPTIMIZED,
+            cachePolicy: { cachePolicyId: OPEN_NEXT_SERVER_CACHE_POLICY_ID },
           },
           imageBehaviorOptions: {
             // We don't need image optimization, so doesn't matter what cache policy we use
