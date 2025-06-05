4758dfdc3adb74bd3d191b26745ada3c01883236
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-06-05 12:47:57 +0300

    OK-1002: Tarkista hakukohteen sijoittelunapin oikeudet organisaatiohierarkiasta

--- src/app/haku/[oid]/hakukohde/[hakukohde]/valintalaskennan-tulokset/components/sijoittelu-status-change-button.tsx
+++ src/app/haku/[oid]/hakukohde/[hakukohde]/valintalaskennan-tulokset/components/sijoittelu-status-change-button.tsx
@@ -1,32 +1,39 @@
 import { OPH_ORGANIZATION_OID } from '@/lib/constants';
-import { UserPermissions } from '@/lib/permissions';
 import { ButtonProps } from '@mui/material';
 import { OphButton } from '@opetushallitus/oph-design-system';
 import { useSijoitteluStatusMutation } from '../hooks/useSijoitteluStatusMutation';
-import { useTranslations } from '@/lib/localization/useTranslations';
+import { TFunction, useTranslations } from '@/lib/localization/useTranslations';
 import { LaskennanValintatapajonoTulosWithHakijaInfo } from '@/hooks/useEditableValintalaskennanTulokset';
+import { checkHasPermission, UserPermissions } from '@/lib/permissions';
+import {
+  useHierarchyUserPermissions,
+  useUserPermissions,
+} from '@/hooks/useUserPermissions';
 
 const SijoitteluButton = ({ ...props }: ButtonProps) => {
   return <OphButton {...props} variant="outlined" />;
 };
 
-export const SijoitteluStatusChangeButton = ({
+export const PureSijoitteluStatusChangeButton = ({
   tarjoajaOid,
   jono,
-  permissions,
   statusMutation,
+  userPermissions,
+  t,
 }: {
   tarjoajaOid: string;
   jono: LaskennanValintatapajonoTulosWithHakijaInfo;
-  permissions: UserPermissions;
   statusMutation: ReturnType<typeof useSijoitteluStatusMutation>;
+  userPermissions: UserPermissions;
+  t: TFunction;
 }) => {
-  const { t } = useTranslations();
+  const hasOphUpdate = checkHasPermission(
+    OPH_ORGANIZATION_OID,
+    userPermissions,
+    'READ_UPDATE',
+  );
 
-  const hasOphUpdate =
-    permissions.writeOrganizations.includes(OPH_ORGANIZATION_OID);
-
-  const hasOrgCrud = permissions.crudOrganizations.includes(tarjoajaOid);
+  const hasOrgCrud = checkHasPermission(tarjoajaOid, userPermissions, 'CRUD');
 
   switch (true) {
     case !jono.valmisSijoiteltavaksi && jono.siirretaanSijoitteluun:
@@ -57,4 +64,28 @@ export const SijoitteluStatusChangeButton = ({
     default:
       return null;
   }
+};
+
+export const SijoitteluStatusChangeButton = ({
+  tarjoajaOid,
+  jono,
+  statusMutation,
+}: {
+  tarjoajaOid: string;
+  jono: LaskennanValintatapajonoTulosWithHakijaInfo;
+  statusMutation: ReturnType<typeof useSijoitteluStatusMutation>;
+}) => {
+  const { t } = useTranslations();
+  const userPermissions = useUserPermissions();
+  const hierarchyUserPermissions = useHierarchyUserPermissions(userPermissions);
+
+  return (
+    <PureSijoitteluStatusChangeButton
+      tarjoajaOid={tarjoajaOid}
+      jono={jono}
+      statusMutation={statusMutation}
+      userPermissions={hierarchyUserPermissions}
+      t={t}
+    />
+  );
 };
