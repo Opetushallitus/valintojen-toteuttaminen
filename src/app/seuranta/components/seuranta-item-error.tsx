a24a710367829610a59f12bbbb0fb70d75afc9d0
Risto Salama <risto.salama@gofore.com>
2025-09-30 13:05:11 +0300

    Kopioi hakukohdetunnisteet clibboardiin toiminto

--- src/app/seuranta/components/seuranta-item-error.tsx
+++ src/app/seuranta/components/seuranta-item-error.tsx
@@ -6,8 +6,16 @@ import { useTranslations } from '@/lib/localization/useTranslations';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getLaskennanYhteenveto } from '@/lib/valintalaskenta/valintalaskenta-service';
 import { ErrorAlert } from '@/components/error-alert';
-import { uniqueBy } from 'remeda';
+import { unique, uniqueBy } from 'remeda';
 import { styled } from '@/lib/theme';
+import { OphButton } from '@opetushallitus/oph-design-system';
+import useToaster from '@/hooks/useToaster';
+
+type ErrorMessage = {
+  hakukohdeOid: string;
+  message: string;
+  key: string;
+};
 
 const StyledMessageContainer = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -24,7 +32,7 @@ const StyledMessage = styled(Box)(({ theme }) => ({
 const ErrorsChildren = ({
   errorMessages,
 }: {
-  errorMessages: Array<{ hakukohdeOid: string; message: string; key: string }>;
+  errorMessages: Array<ErrorMessage>;
 }) => {
   return (
     <StyledMessageContainer>
@@ -44,6 +52,7 @@ export const SeurantaItemError = ({
   seurantaTiedot: SeurantaTiedotLaajennettu;
 }) => {
   const { t } = useTranslations();
+  const { addToast } = useToaster();
 
   const { data: summary } = useSuspenseQuery({
     queryKey: ['seuranta-virhe', seurantaTiedot.uuid],
@@ -63,13 +72,29 @@ export const SeurantaItemError = ({
     (m) => m.hakukohdeOid + m.message,
   );
 
+  function copyToClipBoard(errorMessages: Array<ErrorMessage>) {
+    const hakukohdeOids = unique(errorMessages.map((e) => e.hakukohdeOid));
+    navigator.clipboard.writeText(hakukohdeOids.join('\n'));
+    addToast({
+      key: 'hakukohdeoids-copied-to-clipboard',
+      message: t('seuranta.kopioitu-leikepoydalle'),
+      type: 'success',
+    });
+  }
+
   return (
-    <Box sx={{ gridArea: 'error' }}>
+    <Box sx={{ gridArea: 'error', backgroundColor: 'rgb(250, 238, 234)' }}>
       <ErrorAlert
         title={t('valintalaskenta.valintalaskenta-epaonnistui')}
         messageChildren={<ErrorsChildren errorMessages={summaryErrors} />}
         hasAccordion={summaryErrors?.length > 0}
       />
+      <OphButton
+        sx={{ marginBottom: '5px' }}
+        onClick={() => copyToClipBoard(summaryErrors)}
+      >
+        {t('seuranta.kopioi-leikepoydalle')}
+      </OphButton>
     </Box>
   );
 };
