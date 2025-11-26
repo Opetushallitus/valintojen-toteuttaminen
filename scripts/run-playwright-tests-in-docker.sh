52d18ec4e7835348818f9c8c1563450d4b7f1711
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2025-11-26 15:34:02 +0200

    OY-5308(subOY-5386): playwright docker pnpm --> npx

--- scripts/run-playwright-tests-in-docker.sh
+++ scripts/run-playwright-tests-in-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PLAYWRIGHT_VERSION=$(pnpm list --depth 0 --json @playwright/test | node -e "const fs = require('fs'); const data = JSON.parse(fs.readFileSync(0, 'utf8')); const entry = data.find(pkg => pkg.name === '@playwright/test'); if (!entry || !entry.version) { process.exit(1); } console.log(entry.version.replace(/^@playwright\\/test@/, ''));")
+PLAYWRIGHT_VERSION=$(node -e "console.log(require('@playwright/test/package.json').version)")
+echo "Running Playwright tests in Docker image mcr.microsoft.com/playwright:v${PLAYWRIGHT_VERSION}"
 
 docker run -e CI --mount type=bind,source="$PWD",target=/app --user "$(id -u):$(id -g)" -w /app --ipc=host --net=host mcr.microsoft.com/playwright:v"$PLAYWRIGHT_VERSION" npx playwright test "$@"
